a29601b5aca24275d2903a8fd9c68fd459823bdf
geekymca <63891813+geekymca@users.noreply.github.com>
2020-10-01 11:44:32 +0530

    Update random.cpp
    Provide comments and loop for similar task

--- random.cpp
+++ random.cpp
@@ -1,3 +1,4 @@
+//C++ Program to guess a number 
 #include <iostream>
 
 using namespace std;
@@ -5,6 +6,8 @@ int main()
 {
 	int x;
 	cout <<"I am thinking of a number between 1 and 20, can you guess what it is?";
+	//create a loop for this
+	for(int i=0;i<10;i++){
 	cin >> x;
 
 	if(x==13)
@@ -13,41 +16,4 @@ int main()
 		cout << "You number was too big" << endl;
 	if(x < 13)
 		cout << "Your number was too small" << endl;
-
-	cin >>x ;
-
-	if(x==13)
-		cout<<"You got it right, it is 13" << endl ;
-	if( x > 13)
-		cout << "You number was too big" << endl;
-	if(x < 13)
-		cout << "Your number was too small" << endl;
-	cin>>x;
-	if(x==13)
-		cout<<"You got it right, it is 13" << endl ;
-	if( x > 13)
-		cout << "You number was too big" << endl;
-	if(x < 13)
-		cout << "Your number was too small" << endl;
-	cin>>x;
-	if(x==13)
-		cout<<"You got it right, it is 13" << endl ;
-	if( x > 13)
-		cout << "You number was too big" << endl;
-	if(x < 13)
-		cout << "Your number was too small" << endl;
-	cin>>x;
-	if(x==13)
-		cout<<"You got it right, it is 13" << endl ;
-	if( x > 13)
-		cout << "You number was too big" << endl;
-	if(x < 13)
-		cout << "Your number was too small" << endl;
-	cin>>x;
-	if(x==13)
-		cout<<"You got it right, it is 13" << endl ;
-	if( x > 13)
-		cout << "It was 13" << endl;
-	if(x < 13)
-		cout << "You did not get it right it was 13" << endl;
-	cin>>x;
+	}
